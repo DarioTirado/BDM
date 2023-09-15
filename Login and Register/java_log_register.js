fb951a1a184322f0ef23b0f176e0eb0602b28378
Dario Tirado <xdarksx.514@gmail.com>
2023-09-15 00:39:39 -0600

    Todo_vinculado

---
+++ Login and Register/java_log_register.js
@@ -0,0 +1,17 @@
+
+
+var login = document.getElementById("login");
+
+login.addEventListener("click", function() {
+    // Redirige a la otra página
+    location.href = "/pagInicio.html";
+});
+
+
+
+
+
+
+
+
+
